c60a30fc88c206dc9d1d95ece02920eb4be25a01
engin-rl <engincelikk04@gmail.com>
2024-08-24 12:59:55 +0300

    done

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# This entrypoint file to be used in development. Start by reading README.md
 import medical_data_visualizer
 from unittest import main
 
-# Test your function by calling it here
+# Test
 medical_data_visualizer.draw_cat_plot()
 medical_data_visualizer.draw_heat_map()
 
-# Run unit tests automatically
+# Run unit tests
 main(module="test_module", exit=False)
